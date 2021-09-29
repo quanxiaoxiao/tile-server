cf85651b6c5065297ccd963cd551e0bcb1d991e4
huzhedong <huzd@sunlandgroup.cn>
2021-09-29 10:08:24 +0800

    xxx

--- src/config.js
+++ src/config.js
@@ -3,5 +3,4 @@ const path = require('path');
 module.exports = {
   port: 3000,
   tilePath: path.resolve(__dirname, '..', 'tiles'),
-  // tilePath: path.resolve(os.homedir(), 'run', 'tile-server', 'tiles'),
 };
